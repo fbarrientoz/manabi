f29408219deaed9c5744c8169ec04cd5b7d68e37
fbarrientos <fbarrientos@uabc.edu.mx>
2021-11-19 17:58:56 -0800

    Add project files.

---
+++ Controllers/PictogramaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace manabi.Controllers
+{
+    public class PictogramaController : Controller
+    {
+        // GET: Pictograma
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(manabi.Startup))]
+namespace manabi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
